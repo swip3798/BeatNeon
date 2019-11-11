6ebed462fe643cf3b3b0787f416bc3dd3ddfb854
Christian Schweigel <christian.david.schweigel@sap.com>
2019-11-11 14:05:31 +0100

    Finished the color picker for lights

--- widgets/ColorPicker.gd
+++ widgets/ColorPicker.gd
@@ -1,16 +1,3 @@
 extends Control
 
-var picker: ColorPicker
-var rect: ColorRect
 
-func _ready():
-	picker = get_node("Picker")
-	rect = get_node("ColorRect")
-
-func _process(delta):
-	if picker.raw_mode:
-		picker.raw_mode = false
-
-
-func _on_Picker_color_changed(color):
-	rect.color = color
